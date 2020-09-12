83bca5a85dac96af9d47df34baf03158efb657aa
Guleri24 <abhishekguleri24@gmail.com>
2020-09-12 21:39:39 +0530

    add support for *.txt file of UpperCase letters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,31 @@
 package com.company;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
+    private final static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
         Caesar cipher = new Caesar();
         System.out.println("Encryption order = " + new String(cipher.encrypt));
         System.out.println("Decryption order = " + new String(cipher.decrypt));
-        String secret = "MY CODE IS PLATFORM INDEPENDENT";
-        secret = cipher.encrypt(secret);
-        System.out.println(" Encryption of String : " + secret);
-        secret = cipher.decrypt(secret);
-        System.out.println(" Decryption of String : " +secret);
+        System.out.println("Enter File location: *.txt file containing UpperCase letters =");
+        String fileName = in.nextLine();
+        try {
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(fileName));
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileName));
+            String secret = cipher.encrypt(bis);
+            System.out.println(" Encryption of file values : " + secret);
+            byte[] b = secret.getBytes();
+            bos.write(b);
+            secret = cipher.decrypt(bis);
+            System.out.println(" Decryption of file values : " + secret);
+            b = secret.getBytes();
+            bos.write(b);
+        } catch (Exception e) {
+            System.out.println(" File not found. Write a correct file location");
+            main(args);
+        }
     }
 }
 
@@ -22,27 +37,27 @@ class Caesar {
     protected char[] decrypt = new char[ALPHASIZE];
 
     public Caesar() {
-        for(int i=0; i<ALPHASIZE; i++)
-            encrypt[i] = alpha[(i+3)%ALPHASIZE];
-        for(int i=0; i<ALPHASIZE; i++)
-            decrypt[encrypt[i]-'A'] = alpha[i];
+        for (int i = 0; i < ALPHASIZE; i++)
+            encrypt[i] = alpha[(i) % ALPHASIZE];
+        for (int i = 0; i < ALPHASIZE; i++)
+            decrypt[encrypt[i] - 'A'] = alpha[i];
     }
 
     // Encryption Method
-    public String encrypt(String secret) {
-        char[] mess = secret.toCharArray();
-        for(int i=0; i<mess.length; i++)
-            if(Character.isUpperCase(mess[i]))
-                mess[i] = encrypt[mess[i]-'A'];
-            return new String(mess);
+    public String encrypt(BufferedInputStream secret) {
+        char[] mess = secret.toString().toCharArray();
+        for (int i = 0; i < mess.length; i++)
+            if (Character.isUpperCase(mess[i]))
+                mess[i] = encrypt[mess[i] - 'A'];
+        return new String(mess);
     }
 
     // Decryption Method
-    public String decrypt(String secret) {
-        char[] mess = secret.toCharArray();
-        for(int i=0; i<ALPHASIZE; i++)
-            if(Character.isUpperCase(mess[i]))
-                mess[i] = decrypt[mess[i]-'A'];
-            return new String(mess);
+    public String decrypt(BufferedInputStream secret) {
+        char[] mess = secret.toString().toCharArray();
+        for (int i = 0; i < ALPHASIZE; i++)
+            if (Character.isUpperCase(mess[i]))
+                mess[i] = decrypt[mess[i] - 'A'];
+        return new String(mess);
     }
 }
